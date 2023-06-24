6dfe050b33df9c376867a2dda1806c4a7cdeb0c9
강지혁 <wlgur7238@snu.ac.kr>
2023-06-24 22:43:23 +0900

    basic webflux configuration

---
+++ api/src/main/kotlin/kr/ac/snu/tport/configuration/WebfluxConfiguration.kt
@@ -0,0 +1,32 @@
+package kr.ac.snu.tport.configuration
+
+import kr.ac.snu.tport.domain.user.User
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.MethodParameter
+import org.springframework.web.reactive.BindingContext
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Configuration
+class WebfluxConfiguration: WebFluxConfigurer {
+
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        configurer.addCustomResolver(RequestUserArgumentResolver())
+    }
+
+    private class RequestUserArgumentResolver: HandlerMethodArgumentResolver {
+        override fun supportsParameter(parameter: MethodParameter): Boolean =
+            User::class.java.isAssignableFrom(parameter.parameterType)
+
+        override fun resolveArgument(
+            parameter: MethodParameter,
+            bindingContext: BindingContext,
+            exchange: ServerWebExchange
+        ): Mono<Any> {
+            return User(1, "test", "test").let { Mono.just(it) } // TODO
+        }
+    }
+}
---
+++ api/src/main/kotlin/kr/ac/snu/tport/controller/ReservationController.kt
@@ -0,0 +1,15 @@
+package kr.ac.snu.tport.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
+class ReservationController {
+
+    @RequestMapping("/reserve")
+    suspend fun reserveBus(): String {
+        TODO()
+    }
+
+}
