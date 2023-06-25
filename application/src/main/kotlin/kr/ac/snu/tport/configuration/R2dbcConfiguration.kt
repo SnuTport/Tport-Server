d403aa789d6b2c2fa3e034c8b0674375a552cb08
강지혁 <wlgur7238@snu.ac.kr>
2023-06-26 00:50:07 +0900

    Update R2dbcConfiguration.kt

--- application/src/main/kotlin/kr/ac/snu/tport/configuration/R2dbcConfiguration.kt
+++ application/src/main/kotlin/kr/ac/snu/tport/configuration/R2dbcConfiguration.kt
@@ -4,14 +4,10 @@ import io.r2dbc.spi.ConnectionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.converter.Converter
-import org.springframework.core.io.ClassPathResource
 import org.springframework.data.convert.CustomConversions
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
 import org.springframework.data.r2dbc.dialect.DialectResolver
 import org.springframework.r2dbc.connection.R2dbcTransactionManager
-import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @Configuration
 class R2dbcConfiguration(
