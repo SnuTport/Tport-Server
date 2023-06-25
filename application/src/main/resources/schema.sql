f0745b5b2d0eef92f6df8e5d32034492fbc30b4f
강지혁 <wlgur7238@snu.ac.kr>
2023-06-26 00:45:47 +0900

    schema initializer 삭제

--- application/src/main/resources/schema.sql
+++ application/src/main/resources/schema.sql
@@ -1,77 +1,55 @@
 create table if not exists users
 (
-    id       bigint                              not null auto_increment,
-    name     varchar(255)                        not null,
-    password varchar(255)                        not null,
-    reg_ts   timestamp default CURRENT_TIMESTAMP not null comment '생성시각',
-    upd_ts   timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '수정시각',
+    id       bigint       not null auto_increment,
+    name     varchar(255) not null,
+    password varchar(255) not null,
+    reg_ts           timestamp default CURRENT_TIMESTAMP not null comment '생성시각',
+    upd_ts           timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '수정시각',
     primary key (id),
     unique (name)
 );
 
 create table if not exists paths
 (
-    id               bigint       not null auto_increment,
-    bus_id           bigint       not null,
-    get_on_bus_stop  varchar(255) not null,
+    id          bigint       not null auto_increment,
+    bus_id     bigint       not null,
+    get_on_bus_stop varchar(255) not null,
     get_off_bus_stop varchar(255) not null,
-    fare             bigint       not null,
-    travel_time      bigint       not null,
+    fare        bigint not null ,
+    travel_time_minutes bigint not null,
     primary key (id),
     unique (get_on_bus_stop, get_off_bus_stop, bus_id)
 );
 
 create table if not exists buses
 (
-    id             bigint       not null auto_increment,
-    bus_number     varchar(255) not null,
-    capacity       bigint       not null,
-    departure_time varchar(20)  not null,
+    id          bigint       not null auto_increment,
+    bus_number  varchar(255) not null,
+    capacity    bigint not null,
+    departure_time varchar(20) not null,
     primary key (id),
     unique (bus_number)
 );
 
 create table if not exists bus_bus_stops
 (
-    id                bigint       not null auto_increment primary key,
-    bus_id            bigint       not null,
-    bus_stop_name     varchar(255) not null,
-    arrival_time      varchar(20)  not null,
-    forecasted_demand bigint       not null,
+    id          bigint       not null auto_increment primary key ,
+    bus_id     bigint       not null,
+    bus_stop_name varchar(255) not null,
+    arrival_time varchar(20) not null,
+    forecasted_demand bigint not null,
     unique (bus_id, bus_stop_name)
 );
 
 create table if not exists reservations
 (
-    id               bigint                              not null auto_increment primary key,
-    user_id          bigint                              not null,
-    bus_id           bigint                              not null,
-    bus_stop_name    varchar(255)                        not null,
-    reservation_time datetime(6)                         not null,
-    seat_no          varchar(20)                         not null,
+    id          bigint       not null auto_increment primary key ,
+    user_id     bigint       not null,
+    bus_id     bigint       not null,
+    bus_stop_name varchar(255) not null,
+    reservation_time datetime(6) not null,
+    seat_no varchar(20) not null,
     reg_ts           timestamp default CURRENT_TIMESTAMP not null comment '생성시각',
     upd_ts           timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '수정시각',
     unique (bus_id, bus_stop_name)
 );
-
-create table if not exists path_groups
-(
-    id               bigint       not null auto_increment,
-    get_on_bus_stop  varchar(255) not null,
-    get_off_bus_stop varchar(255) not null,
-    fare             bigint       not null,
-    travel_time      bigint       not null,
-    primary key (id),
-    unique (get_on_bus_stop, get_off_bus_stop)
-);
-
-create table if not exists sub_paths
-(
-    id               bigint       not null auto_increment,
-    get_on_bus_stop  varchar(255) not null,
-    get_off_bus_stop varchar(255) not null,
-    path_group_id    bigint       not null,
-    bus_id           bigint       null,
-    type             varchar(10)  null,
-    primary key (id)
-)
