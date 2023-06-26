445399133099a1a9f263cd3ae799bc19f80b435c
강지혁 <wlgur7238@snu.ac.kr>
2023-06-27 01:12:00 +0900

    단건 조회 추가

--- batch/src/test/kotlin/kr/ac/snu/tport/processor/ExcelUploaderTest.kt
+++ batch/src/test/kotlin/kr/ac/snu/tport/processor/ExcelUploaderTest.kt
@@ -1,11 +1,13 @@
 package kr.ac.snu.tport.processor
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Disabled
 class ExcelUploaderTest @Autowired constructor(
     private val uploader: PathGroupExcelUploader
 ) {
