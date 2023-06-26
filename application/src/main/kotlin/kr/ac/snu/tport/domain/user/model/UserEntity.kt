e78c03f82b91e619236cf8ed848f0d82ca96940e
강지혁 <wlgur7238@snu.ac.kr>
2023-06-26 13:16:25 +0900

    fix : reg_ts access error

--- application/src/main/kotlin/kr/ac/snu/tport/domain/user/model/UserEntity.kt
+++ application/src/main/kotlin/kr/ac/snu/tport/domain/user/model/UserEntity.kt
@@ -2,6 +2,7 @@ package kr.ac.snu.tport.domain.user.model
 
 import kr.ac.snu.tport.infra.crypto.EncryptedString
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.ReadOnlyProperty
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
@@ -17,8 +18,10 @@ data class UserEntity(
     val password: EncryptedString
 ) {
     @Column("reg_ts")
-    val regTs: LocalDateTime = LocalDateTime.now()
+    @ReadOnlyProperty
+    var regTs: LocalDateTime = LocalDateTime.now()
 
     @Column("upd_ts")
-    val updTs: LocalDateTime = LocalDateTime.now()
+    @ReadOnlyProperty
+    var updTs: LocalDateTime = LocalDateTime.now()
 }
